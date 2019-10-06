8ac5255f1fa4bc2676e802f60cba5c4fd1eae319
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-06 20:14:32 +0900

    デプロイについての設定を記載するファイルを編集(config/deploy.rb)
    自動デプロイについて設定を記載した
    config/deploy.rbにはここには、production環境、staging環境どちらにも当てはまる設定を記述することになる
    具体的には下記の項目を参照
    アプリケーション名
    gitのレポジトリ
    利用するSCM
    タスク
    それぞれのタスクで実行するコマンド

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,36 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock '3.11.2'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# Capistranoのログの表示に利用
+set :application, 'mercari_gryffindor'
+
+# どのリポジトリからアプリをpullするか指定
+set :repo_url, 'git@github.com:yamawo/mercari_gryffindor.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/MercariGryffindorTeamA.pem']
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
