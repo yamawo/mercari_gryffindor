71e26a9a06b38937d54542ca61e1b60a562cb486
nakayuji <xiongshizhongsi@gmail.com>
2019-10-06 17:28:09 +0900

    ユーザースペックファイルの訂正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
         it "validationのテストします。Usersの全てのカラムが対象です。" do
             user = User.new(last_name: "")
             user.valid?
-            expect(user.errors[:last_name]).to include("Can't be blank")
+            expect(user.errors[:last_name]).to include("can't be blank")
         end
     end
 end
