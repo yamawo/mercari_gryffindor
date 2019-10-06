6a30c6a414284791bcbbdf431036b9c69e14c435
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-10-06 13:11:04 +0900

    まこなりさん

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: "makonari#index"
+  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
