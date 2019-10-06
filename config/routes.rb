709f522e40fe351242ab1b2d20dcf6b6c343e42b
kohama66 <l.my.me666666@gmail.com>
2019-10-06 15:11:43 +0900

    routeにproduct,indexを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:index]
 end
