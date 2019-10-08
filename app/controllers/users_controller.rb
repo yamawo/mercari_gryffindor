91a3dc3f2397ca22264863a859e0672dff68fb73
yamawo <y7a3m1asaki@gmail.com>
2019-10-08 21:26:11 +0900

    step3作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
-
+    before_action :set_year, :set_month, :set_day
+    
     def step3
         @user = User.new()
     end
     
+
+    def set_year
+        years = []
+        for year in 1900..2019 do
+            years << year 
+        end
+        new_years = years.reverse
+        @year = new_years
+    end
+
+    def set_month
+        months = []
+        for month in 1..12 do
+            months << month
+        end
+        @month = months        
+    end
+
+    def set_day
+        days = []
+        for day in 1..31 do
+            days << day
+        end
+        @day = days
+    end
 end
