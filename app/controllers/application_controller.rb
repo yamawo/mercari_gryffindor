addf7e2255f5d93e4262631623d975522d132c13
kohama66 <l.my.me666666@gmail.com>
2019-10-09 10:38:38 +0900

    searsc-product

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :basic_auth, if: :production?
-
+  before_action :search_product
   # private
 
   def production?
@@ -12,5 +12,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == Rails.application.credentials[:basic_auth][:user] && password == Rails.application.credentials[:basic_auth][:password]
     end
+  end
+
+  def search_product
+    @q = Product.ransack(params[:q])
+    @products = @q.result(distinct: true)
   end
 end
