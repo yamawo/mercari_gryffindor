920e482bd2c81f284393b65974c2923058984076
yamawo <y7a3m1asaki@gmail.com>
2019-10-10 14:29:52 +0900

    崩れたstep3.scssを修正途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,24 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
     before_action :set_year, :set_month, :set_day
     layout "users_layout"
     
     def step3
+        # session[:nickname] = user_params[:nickname]
+        # session[:email] = user_params[:email]
+        # session[:password] = user_params[:password]
+        # session[:password_confirmation] = user_params[:password_confirmation]
+        # session[:last_name] = user_params[:last_name]
+        # session[:first_name] = user_params[:first_name]
+        # session[:last_name_kana] = user_params[:last_name_kana]
+        # session[:first_name_kana] = user_params[:first_name_kana]
+        # session[:birthdate_year] = user_params[:birthdate_year]
+        # session[:birthdate_month] = user_params[:birthdate_month]
+        # session[:birthdate_day] = user_params[:birthdate_day]
+        # session[] = user_params[]
+        # session[] = user_params[]
         @user = User.new()
     end
-    
-=======
-  before_action :set_year, :set_month, :set_day
 
-  def step3
-    session[:nickname] = user_params[:nickname]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password]
-    session[:password_confirmation] = user_params[:password_confirmation]
-    session[:last_name] = user_params[:last_name]
-    session[:first_name] = user_params[:first_name]
-    session[:last_name_kana] = user_params[:last_name_kana]
-    session[:first_name_kana] = user_params[:first_name_kana]
-    session[:birthdate_year] = user_params[:birthdate_year]
-    session[:birthdate_month] = user_params[:birthdate_month]
-    session[:birthdate_day] = user_params[:birthdate_day]
-    session[] = user_params[]
-    session[] = user_params[]
-    @user = User.new()
-  end
-
->>>>>>> 31ec68f519d673754b5ace0e1e22c3f447bbefa9
     def step4
       session[] = user_params[]
         @user = User.new()
@@ -65,33 +56,33 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(
-      :last_name,
-      :first_name,
-      :last_name_kana,
-      :first_name_kana,
-      :birthdate_year,
-      :birthdate_month,
-      :birthdate_day,
-      :email,
-      :phone_number,
-      :password,
-      :nickname,
-      :text,
-      :image,
-      :good,
-      :normal,
-      :bad,
-      address_attributes: [
-        :id,
-        :postal_code,
-        :address_prefecture,
-        :address_city,
-        :address_number,
-        :address_building,
-        :address_phone,
-      ]
-    )
-  end  
+#   def user_params
+#     params.require(:user).permit(
+#       :last_name,
+#       :first_name,
+#       :last_name_kana,
+#       :first_name_kana,
+#       :birthdate_year,
+#       :birthdate_month,
+#       :birthdate_day,
+#       :email,
+#       :phone_number,
+#       :password,
+#       :nickname,
+#       :text,
+#       :image,
+#       :good,
+#       :normal,
+#       :bad,
+#       address_attributes: [
+#         :id,
+#         :postal_code,
+#         :address_prefecture,
+#         :address_city,
+#         :address_number,
+#         :address_building,
+#         :address_phone,
+#       ]
+#     )
+#   end  
 end
