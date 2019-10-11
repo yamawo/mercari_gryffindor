ec69b5a6c4e819dc1481acd413f2d747cf0611d3
kohama66 <l.my.me666666@gmail.com>
2019-10-11 12:29:19 +0900

    登録フォーム　バリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,15 @@
 class Address < ApplicationRecord
 
-  belongs_to :user
+  belongs_to :user, optional: true
 
-  with_options presence: true do
-         validates :postal_code
-         validates :address_prefecture
-         validates :address_city
-         validates :address_number
-  end
+  # with_options presence: true do
+  #     validates :last_name
+  #     validates :first_name
+  #     validates :last_name_kana
+  #     validates :first_name_kana
+  #     validates :postal_code
+  #     validates :address_prefecture
+  #     validates :address_city
+  #     validates :address_number
+  # end
 end
