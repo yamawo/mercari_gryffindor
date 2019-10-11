356d1a487456641505270dd0b392929bfbec4e09
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-11 16:27:30 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_082651) do
+ActiveRecord::Schema.define(version: 2019_10_10_083358) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
