5c4b17af95582ca132f400949323c8659aeaf288
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-13 20:23:48 +0900

    Update users_controller.rb
    不要なコメントアウトを修正
    step6アクション内でsessionを使い、user_paramsの中にphone_numberを入れるように修正した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,70 +9,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # def step4 
-  #   @user = User.new
-  #   session[:nickname] = user_params[:nickname]
-  #   session[:email] = user_params[:email]
-  #   session[:password] = user_params[:password]
-  #   session[:password_confirmation] = user_params[:password_confirmation]
-  #   session[:last_name] = user_params[:last_name]
-  #   session[:first_name] = user_params[:first_name]
-  #   session[:last_name_kana] = user_params[:last_name_kana]
-  #   session[:first_name_kana] = user_params[:first_name_kana]
-  #   session[:birthdate_year] = user_params[:birthdate_year]
-  #   session[:birthdate_month] = user_params[:birthdate_month]
-  #   session[:birthdate_day] = user_params[:birthdate_day]
-  # end
-
   def step4
     @user = User.new
-    # ここにはpostal_codeは入ってない
     session[:user_params] = user_params
     
   end
 
   def step6
+    session[:user_params][:phone_number] = user_params[:phone_number]
     @user = User.new
     @user.build_address
-    puts session[:user_params]
   end
 
   def step7
     @user = User.new
-    # 下記の中にpostal_codeが入ってる
     session[:address_attributes1] = user_params[:address_attributes]
     puts session[:user_params]
-    # session[:postal_code] = user_params[:postal_code]
-    # session[:address_prefecture] = user_params[:address_prefecture]
-    # session[:address_city] = user_params[:address_city]
-    # session[:address_number] = user_params[:address_number]
-    # session[:last_name] = user_params[:last_name]
-    # session[:first_name] = user_params[:first_name]
-    # session[:last_name_kana] = user_params[:last_name_kana]
-    # session[:first_name_kana] = user_params[:first_name_kana]
-    # session[:address_building] = user_params[:address_building]
-    # session[:address_phone] = user_params[:address_phone]
-    
-    #     PayjpとCardのデータベースを作成
-    #     Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
-
-    #     if params['payjp-token'].blank?
-    #         redirect_to action: "new"
-    #     else
-    #         # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録する
-    #         customer = Payjp::Customer.create(
-    #             description: 'test', # 書かなくてもいい。PAY.JPの顧客情報に表示する概要
-    #             email: current_user.email, # current_user.emailなのかな？
-    #             card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐づけて永久保存する
-    #             metadata: {user_id: current_user.id} # 書かなくてもOK。
-    #         )
-    #         @card = Credit.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-    #         if @card.save
-    #             redirect_to action: "create"
-    #         else
-    #             render "/"
-    #         end
-    #     end
   end
 
   def set_year
@@ -89,7 +41,7 @@ class UsersController < ApplicationController
     for month in 1..12 do
         months << month
     end
-    @month = months        
+    @month = months
   end
 
   def set_day
@@ -102,21 +54,21 @@ class UsersController < ApplicationController
 
   def step8
     @user = User.new(session[:user_params])
-    @user.build_address(user_params[:address_attributes1])
+    @user.build_address(session[:address_attributes1])
     if @user.save
         session[:id] = @user.id
-        PayjpとCardのデータベースを作成
+        # PayjpとCardのデータベースを作成
         Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_SECRET_KEY]
 
         if params['payjp-token'].blank?
-            redirect_to action: "new"
+            redirect_to "/"
         else
             # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録する
             customer = Payjp::Customer.create(
                 description: 'test', # 書かなくてもいい。PAY.JPの顧客情報に表示する概要
-                email: @user.email, # current_user.emailなのかな？
+                email: @user.email,
                 card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐づけて永久保存する
-                metadata: {user_id: @user.id} # 書かなくてもOK。
+                metadata: {user_id: @user.id}
             )
             @card = Credit.new(user_id: @user.id, customer_id: customer.id, card_id: customer.default_card)
             if @card.save
@@ -130,68 +82,11 @@ class UsersController < ApplicationController
     end
   end
 
-  # def validates_step3
-  #   session[:nickname] = user_params[:nickname]
-  #   session[:email] = user_params[:email]
-  #   session[:password] = user_params[:password]
-  #   session[:password_confirmation] = user_params[:password_confirmation]
-  #   session[:last_name] = user_params[:last_name]
-  #   session[:first_name] = user_params[:first_name]
-  #   session[:last_name_kana] = user_params[:last_name_kana]
-  #   session[:first_name_kana] = user_params[:first_name_kana]
-  #   session[:birthdate_year] = user_params[:birthdate_year]
-  #   session[:birthdate_month] = user_params[:birthdate_month]
-  #   session[:birthdate_day] = user_params[:birthdate_day]
-  #   @user = User.new(
-  #     nickname: session[:nickname],
-  #     email: session[:email],
-  #     password: session[:password],
-  #     password_confirmation: session[:password_confirmation],
-  #     last_name: session[:last_name],
-  #     first_name: session[:first_name],
-  #     last_name_kana: session[:last_name_kana],
-  #     first_name_kana: session[:first_name_kana],
-  #     birthdate_year: session[:birthdate_year],
-  #     birthdate_month: session[:birthdate_month],
-  #     birthdate_day: session[:birthdate_day],
-  #     phone_number: "09012345678"
-  #   )
-  #   render "/users/step3" unless @user.valid?(:validates_step3)
-  # end
-
   def validates_step3
     session[:user_params] = user_params
     @user = User.new(session[:user_params])
     render "/users/step3" unless @user.valid?(:validates_step3)
   end
-
-  # def validates_step6
-  #   # @user = User.new
-  #   session[:last_name] = user_params[:last_name]
-  #   session[:first_name] = user_params[:first_name]
-  #   session[:first_name_kana] = user_params[:first_name_kana]
-  #   session[:last_name_kana] = user_params[:last_name_kana]
-  #   session[:postal_code] = user_params[:postal_code]
-  #   session[:address_prefecture] = user_params[:address_prefecture]
-  #   session[:address_city] = user_params[:address_city]
-  #   session[:address_number] = user_params[:address_number]
-  #   @user = User.new( 
-  #     last_name: session[:last_name],
-  #     first_name: session[:first_name],
-  #     first_name_kana: session[:first_name_kana],
-  #     last_name_kana: session[:last_name_kana],
-  #     postal_code: session[:postal_code],
-  #     address_prefecture: session[:address_prefecture],
-  #     address_city: session[:address_city],
-  #     address_number: session[:address_number]
-  #   )
-  #   render "/users/step6" unless @user.valid?(:validates_step6)
-  #   unless @user.valid?(:validates_step6)
-  #     @user = User.new
-  #     @user.build_address
-  #     render "/users/step6"
-  #   end  
-  # end
 
   def validates_step6
     session[:address_attributes1] = user_params[:address_attributes]
