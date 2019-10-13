8a2243e6ef5c0a71d38e71a08ae2f7368e6b4235
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-13 20:24:51 +0900

    Update user.rb
    creditモデルとアソシエーションの関係になるように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
   has_one :address
+  has_one :credit
   accepts_nested_attributes_for :address
 
   with_options presence: true do
@@ -11,7 +12,7 @@ class User < ApplicationRecord
        validates :first_name, on: :validates_step3
        validates :last_name_kana, on: :validates_step3
        validates :first_name_kana, on: :validates_step3
-     #   validates :phone_number, on: :validates_step3
+       validates :phone_number, on: :validates_step3
        validates :nickname, on: :validates_step3
        validates :password_confirmation, on: :validates_step3
        validates :birthdate_year, on: :validates_step3
