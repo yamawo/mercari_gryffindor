b1f1056310e01a51924f1a6a7298adab95509e2f
nakayuji <xiongshizhongsi@gmail.com>
2019-10-13 21:16:46 +0900

    商品詳細ページ大枠完成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,4 @@
 class ProductsController < ApplicationController
-
-  
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   resources :users, only: [:mypage, :logout] do
     collection do
       get "profile"
