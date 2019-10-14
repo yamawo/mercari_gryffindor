520a161e18ce1dbd06eda6f120e5859e27b8e48e
nakayuji <xiongshizhongsi@gmail.com>
2019-10-14 20:33:20 +0900

    商品詳細　user product image  データベースから持ち出し完了

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
   def show
+    @products = Product.find(1)
+    @users = User.find(1)
+    @images = Image.find(2)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_11_014514) do
+ActiveRecord::Schema.define(version: 2019_10_14_102815) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 2019_10_11_014514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_credits_on_user_id"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image"
+    t.integer "product_id"
   end
 
   create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
