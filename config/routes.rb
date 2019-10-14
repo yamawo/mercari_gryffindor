a49c8ec20d4f31a5f01df7b819b228df3121e017
nakayuji <xiongshizhongsi@gmail.com>
2019-10-14 14:11:50 +0900

    サイダー　修正

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     end
   end
   
-  resources :products, only: [:index, :index]
+  resources :products, only: [:index, :show]
   
   resources :users do
     collection do
