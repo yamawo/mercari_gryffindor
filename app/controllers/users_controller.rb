19467204814e76a976d2972406652273e1a5262e
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-16 11:35:08 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,17 @@ class UsersController < ApplicationController
 
   def profile
   end
-  def mypage
 
+  def mypage
   end
 
   def logout
-    
   end
   
   def confirmation
     @address = Address.new
   end
+  
   def card_registration
   end
 end
