4f1d3858c9855a883f2e66f6af3a0d8101cf30df
yamawo <y7a3m1asaki@gmail.com>
2019-10-17 20:07:12 +0900

    preview.js作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,25 +5,45 @@ class ProductsController < ApplicationController
   end
 
   def new
+    require "base64"
     @product = Product.new
     @product.product_images.build
     render layout: "selling"
   end
 
-  def creare
+  def create
+    require "base64"
     @product = Product.new(product_params)
-    @product.product_images.build(product_params[:product_images_attributes])
     if @product.save
+      product_images_params[:images].each do |image|
+        @product.product_images.build   # buildをすることで、saveした際にアソシエーションした先にも値を保存する
+        product_image = @product.product_images.new(image: image)
+        product_image.save
+      end
+      respond_to do |format|
+        format.json
+      end
     end
   end
 
+  def edit
+    require "base64"
+  end
+
   def privacy_policy
-      
   end
 
   private 
   def product_params
-    params.require(:product).permit(:name, :price, :text, :status, :stage, :delivery_responsivility, :delivery_way, :delivery_area, :delivery_day, :category_id, :brand_id, product_images_attributes: [:id, :image, :product_id])
+    params.require(:product).permit(:name, :price, :text, :status, :stage, :delivery_responsivility, :delivery_way, :delivery_area, :delivery_day, :category_id, :brand_id)
+  end
+
+  def registered_image_params
+    params.require(:registered_images_ids).permit({ids: []})
+  end
+
+  def product_images_params
+    params.require(:product_images).permit({images: []})
   end
   
 end
