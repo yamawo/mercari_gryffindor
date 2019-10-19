293f5ec04c2ffbda9e23b02ed20bc1a884a62c18
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-10-19 12:23:48 +0900

    ブランドサーチ

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,15 +1,32 @@
 $(function() {
-  $("#brand").on("input",(){
-    var input = $("#brand").val();
-    console.log(input)
+  
+  function appendBrand(brand) {
+    var search_result = $(".selling__main__sec__content__form__write__content__box__group__select__search");
+    var html = `<li class="selling__main__sec__content__form__write__content__box__group__select__search__ele" id="${brand.id}">
+                  ${brand.name}
+                </li>`
+    search_result.append(html);
+  }
+  $(document).on('input', "#brand_list", function(){//動的に追加した要素はdocumentで
+    var input = $('#brand_list').val();
+    
     if (input.length !== 0){
 
       $.ajax({
-        url: "/producs/search",
-        data: { keyword: input },
-        dataType: "json"
+      url: "/products/search",
+      data: { keyword: input },
+      dataType: "json"
       })
-      .done
+      .done(function(brands){
+      $(".selling__main__sec__content__form__write__content__box__group__select__search").empty();
+      if(brands.length !== 0){
+        brands.forEach(function(brand){
+          appendBrand(brand);
+          
+        })
+        
+      }
+      })
     }
   })
 })
