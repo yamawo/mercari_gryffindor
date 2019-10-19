744a6e3fbd1e17c340e1f383a8f5c252d8463050
nakayuji <xiongshizhongsi@gmail.com>
2019-10-19 13:13:51 +0900

    seeds に商品詳細のため必要なすべてのデータ打ち込み

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,8 @@ class ProductsController < ApplicationController
   def index
     ladies = Category.find_by(name: "レディース")
     @ladies = Product.where(category_id: ladies.indirects.ids).limit(10)
-    # chanel = Brand.find_by(name: "シャネル")
-    # @chanel = Product.where(brand_id: chanel.id).limit(10)
+    chanel = Brand.find_by(name: "シャネル")
+    @chanel = Product.where(brand_id: chanel.id).limit(10)
   end
 
   def new
