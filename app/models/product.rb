0eed49dd7e4cad91daac0b15da7d2c455c679dbf
nakayuji <xiongshizhongsi@gmail.com>
2019-10-20 16:03:13 +0900

    enum をProduct.rbに追記

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :product_images
   accepts_nested_attributes_for :product_images
+  enum status: [["---",""], ["新品",0], ["未使用に近い",1], ["目立った傷や汚れなし",2], ["やや傷や汚れあり",4], ["傷や汚れあり",4], ["全体的に状態が悪い",5]]
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_081908) do
+ActiveRecord::Schema.define(version: 2019_10_20_060358) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2019_10_19_081908) do
     t.string "name", null: false
     t.integer "price", null: false
     t.text "text", null: false
-    t.integer "status", null: false
     t.string "delivery_responsivility", null: false
     t.string "delivery_way", null: false
     t.string "delivery_area", null: false
@@ -74,8 +73,10 @@ ActiveRecord::Schema.define(version: 2019_10_19_081908) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.bigint "brand_id"
+    t.integer "status", default: 0, null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["status"], name: "index_products_on_status"
   end
 
   create_table "sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
