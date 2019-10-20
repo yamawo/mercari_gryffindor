207aedc03eddd1ce5f6ddfd2fb4119d64b95dae4
kohama66 <l.my.me666666@gmail.com>
2019-10-20 22:01:05 +0900

    sns devise routes 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: "products#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  } 
 
   resources :products, only: [:new, :create, :index, :show] do
     collection do
@@ -13,12 +17,6 @@ Rails.application.routes.draw do
   end
 
   resources :address 
-
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  } 
 
   resources :users do
     collection do
