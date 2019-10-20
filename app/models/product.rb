f49229e8e9288c318c5d7dfacf2567b7a472b516
nakayuji <xiongshizhongsi@gmail.com>
2019-10-20 16:48:38 +0900

    enamでのstatusの保存機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,5 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :product_images
   accepts_nested_attributes_for :product_images
-  enum status: [["---",""], ["新品",0], ["未使用に近い",1], ["目立った傷や汚れなし",2], ["やや傷や汚れあり",4], ["傷や汚れあり",4], ["全体的に状態が悪い",5]]
-  
+  enum status: %i( --- 新品 未使用に近い 目立った傷や汚れなし やや傷や汚れあり 傷や汚れあり 全体的に状態が悪い )
 end
