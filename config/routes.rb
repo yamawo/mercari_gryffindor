51145ef949f07355f4b83ed1c667349c6b745730
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-20 18:30:24 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: "products#index"
- 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products, only: [:new, :create, :index] do
