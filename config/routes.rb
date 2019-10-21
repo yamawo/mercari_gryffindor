e8497c72ade2a1255b64d16109d75990f3960db9
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-21 19:50:41 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       get "create_category_grandchildren"
       get "search_size"
       get "search"
-      get "confirmation"
+      get "buy_confirmation"
     end
   end
 
