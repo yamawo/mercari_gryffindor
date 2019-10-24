e59e0535f388de47a5848928908ffd9716385d35
SnakeY9 <yuya.soccerlove17@gmail.com>
2019-10-24 19:30:26 +0900

    seed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ size_blocks[1168..1297].each_with_index do |size_block, i|
 end
 
 #カテゴリーデータ取得
-file = File.open("category-index.htm")
+file = File.open("カテゴリー一覧 - メルカリ スマホでかんたん フリマアプリ.htm")
 doc = Nokogiri::HTML(file)
 
 parent_category_blocks = doc.search(".category-list-individual-box")
