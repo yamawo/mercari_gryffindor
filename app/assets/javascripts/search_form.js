ac6a0f784081cc9443f649efeea70dd83a694a5f
kohama66 <l.my.me666666@gmail.com>
2019-10-25 16:51:58 +0900

    カテゴリーフォーム孫要素
    カテゴリーフォームの孫要素を表示機能を追加

--- app/assets/javascripts/search_form.js
+++ app/assets/javascripts/search_form.js
@@ -1,10 +1,15 @@
 $(function(){
 
   let get_HTML = function(){
-  let html = `<select name="q[category_id]" id="q_category_id" class="category_children">`
+  let html = `<select class="category_children" name="q[category_id]" id="q_category_id">`
   return html;
   };
 
+  let get_HTML_indirects = function(){
+  let html = `<select class="category_indirects" name="q[category_id]" id="q_category_id">`
+  return html;
+  };
+  
   function get_option(data){
     let html = `<option value="${data.id}">${data.name}</option>`
     return html;
@@ -19,60 +24,34 @@ $(function(){
       data: {id: id}
     })
     .done(function(datas){
-      if ($("select").hasClass("category_children")){
         $(".category_children").remove();
         $(".search_for-main__side-bar--category").append(get_HTML)
           datas.forEach(function(data){
             option = get_option(data);
             $(".category_children").append(option)
         });
-      }
-      else{
-        $(".search_for-main__side-bar--category").append(get_HTML)
-          datas.forEach(function(data){
-            option = get_option(data);
-            $(".category_children").append(option)
-        });
-      }
     })
     .fail(function(){
-      console.log("ng")
     })
   });
 
-  // $(".category_children").change(function(){
-  //   console.log("ok")
-  //   let id = $(this).val();
-  //   $.ajax({
-  //     url: "/search_form_lv2",
-  //     type: "get",
-  //     datatype: "json",
-  //     data: {id: id}
-  //   })
-  //   .done(function(datas){
-  //     if ($("select").hasClass("category_children")){
-  //       console.log("ok")
-  //     }
-  //     else{
-  //       $(".search_for-main__side-bar--category").append(get_HTML)
-  //         datas.forEach(function(data){
-  //           option = get_option(data);
-  //           $(".category_children").append(option)
-  //       });
-  //     }
-  //   })
-  //   .fail(function(){
-  //     console.log("ng")
-  //   })
-  // });
-
-
-
-
-
-
-
-
-
-
+    $(document).on("change",".category_children", function(){
+    let id = $(this).val();
+    $.ajax({
+      url: "/search_form_lv2",
+      type: "get",
+      datatype: "json",
+      data: {id: id}
+    })
+    .done(function(datas){
+      $(".search_for-main__side-bar--category").append(get_HTML_indirects)
+      datas.forEach(function(data){
+        option = get_option(data);
+        $(".category_indirects").append(option)
+      })
+    })
+    .fail(function(){
+    })
+  });
+  
 })
