ff8ef76a41267a0b1c901a7e068f85bdd59bf261
kohama66 <l.my.me666666@gmail.com>
2019-10-25 14:02:45 +0900

    カテゴリフォーム
    子要素カテゴリフォーム、親要素を更新すれば中身も変わるように変更

--- app/assets/javascripts/search_form.js
+++ app/assets/javascripts/search_form.js
@@ -1,32 +1,78 @@
 $(function(){
 
   let get_HTML = function(){
-  let html = `<select name="q[category_id]" id="q_category_id">`
-                `<option value="">全て</option>`
+  let html = `<select name="q[category_id]" id="q_category_id" class="category_children">`
   return html;
   };
-  let get_option = function(data){
-    let html = `<option value="${data.id}">全て</option>`
 
+  function get_option(data){
+    let html = `<option value="${data.id}">${data.name}</option>`
+    return html;
   }
 
-
-  $("#q_category_id").change(function(){
-    
+  $(".category_select").change(function(){
     let id = $(this).val();
     $.ajax({
       url: "/search_form_lv2",
       type: "get",
       datatype: "json",
-      data: { id: id }
+      data: {id: id}
     })
-    .done(function(data){
-      console.log(data)
+    .done(function(datas){
+      if ($("select").hasClass("category_children")){
+        $(".category_children").remove();
+        $(".search_for-main__side-bar--category").append(get_HTML)
+          datas.forEach(function(data){
+            option = get_option(data);
+            $(".category_children").append(option)
+        });
+      }
+      else{
+        $(".search_for-main__side-bar--category").append(get_HTML)
+          datas.forEach(function(data){
+            option = get_option(data);
+            $(".category_children").append(option)
+        });
+      }
     })
     .fail(function(){
       console.log("ng")
     })
   });
-})
 
-// $(".search_for-main__side-bar--category").append(get_HTML)
+  // $(".category_children").change(function(){
+  //   console.log("ok")
+  //   let id = $(this).val();
+  //   $.ajax({
+  //     url: "/search_form_lv2",
+  //     type: "get",
+  //     datatype: "json",
+  //     data: {id: id}
+  //   })
+  //   .done(function(datas){
+  //     if ($("select").hasClass("category_children")){
+  //       console.log("ok")
+  //     }
+  //     else{
+  //       $(".search_for-main__side-bar--category").append(get_HTML)
+  //         datas.forEach(function(data){
+  //           option = get_option(data);
+  //           $(".category_children").append(option)
+  //       });
+  //     }
+  //   })
+  //   .fail(function(){
+  //     console.log("ng")
+  //   })
+  // });
+
+
+
+
+
+
+
+
+
+
+})
