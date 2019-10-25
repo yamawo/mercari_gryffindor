d41127c7e78ceef36e942e4a4f4ee92e5cb305c9
kohama66 <l.my.me666666@gmail.com>
2019-10-26 00:42:40 +0900

    カテゴリ検索機能追加
    親カテゴリを選び、孫まで全て取得可能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,20 +16,35 @@ class ApplicationController < ActionController::Base
   def search_product
     @q = Product.ransack(params[:q])
     @products = @q.result(distinct: true)
-    # @category = []
     @category = Category.where(ancestry: nil)
   end
 
   def search_for
-
-    q = search_params[:category_id_eq] 
-    q = q.indirects
-    binding.pry
     @q = Product.search(search_params)
-    # binding.pry
     @products = @q.result(distinct: true)
     @count = @products.count.to_s
   end
+
+  def select_search
+    q = Category.find(search_params[:category_id_eq]) 
+    q_indirects = q.indirects
+    q_children = q.children
+    # if q_indirects.present?
+    session[:search_params] = search_params
+    session[:search_params].delete(:category_id_eq)
+    session[:search_params][:category_id_eq_any] = q_indirects.ids
+    
+
+    # end
+    @q = Product.ransack(session[:search_params])
+    # binding.pry
+    @products = @q.result(distinct: true)
+    @count = @products.count.to_s
+    render "application/search_for"
+  end
+
+
+
 
   def search_form_lv2
    category = Category.find_by(id: params[:id])
@@ -47,6 +62,5 @@ class ApplicationController < ActionController::Base
   end
 
   def application
-    
   end
 end
