df7061890e68e4d577fa83fabfa91bd05524f570
kohama66 <l.my.me666666@gmail.com>
2019-10-27 20:06:59 +0900

    リセットボタン作成

---
+++ app/assets/javascripts/select_form_reset.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  $(".search_for-main__side-bar--button--reset").on("click", function(e){
+    e.preventDefault();
+    clearForm(".search_form_side");
+    $(".category_children").remove();
+    $(".category_indirects").remove();
+  })
+
+  function clearForm (form) {
+    $(form)
+      .find("input, select, textarea")
+      .not(":button, :submit, :reset, :hidden")
+      .val("")
+      .prop("checked", false)
+      .prop("selected", false)
+    ;
+  }
+})
