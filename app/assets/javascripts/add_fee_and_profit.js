136718bc10a78d80120eaca58fe34178b0f59c26
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-10-27 11:58:38 +0900

    販売手数料と販売利益の修正

--- app/assets/javascripts/add_fee_and_profit.js
+++ app/assets/javascripts/add_fee_and_profit.js
@@ -1,20 +1,31 @@
 $(function(){
   var $input = $("#product_price");
-  var $output1 = $("#fee");
-  var $output2 = $("#profit");
+  var $fee = $("#fee");
+  var $profit = $("#profit");
   $input.keyup("#product_price", function(event) {
     var value = $input.val();
-    var fee = Math.floor(value * 0.1);
-    $output1.html('¥' + fee)
+    if(value >= 300){
+      var fee = Math.floor(value * 0.1);
+    $fee.html('¥' + fee)
     var profit = Math.floor(value * 0.9);
-    $output2.text('¥' + profit);
+    $profit.text('¥' + profit);
+    }else{
+      $fee.text("−")
+      $profit.text("−")
+    }
+
   })
   if (window.location.href.match(/\/products\/\d+\/edit/)){
-      var value = $input.val();
+    var value = $input.val();
+    if(value >= 300){
       var fee = Math.floor(value * 0.1);
-      $output1.html('¥' + fee)
-      var profit = Math.floor(value * 0.9);
-      $output2.text('¥' + profit);
+    $fee.html('¥' + fee)
+    var profit = Math.floor(value * 0.9);
+    $profit.text('¥' + profit);
+    }else{
+      $fee.text("−")
+      $profit.text("−")
+    }
 
   }
 })
