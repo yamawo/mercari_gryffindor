f4d5b4a3b9c3b2fbbe8f78d88b59bf0f9fe60640
kohama66 <l.my.me666666@gmail.com>
2019-10-27 11:59:16 +0900

    select_search_lv2編集
    ターボリンクスロードの記述を消去

--- app/assets/javascripts/select_brand_reset.js
+++ app/assets/javascripts/select_brand_reset.js
@@ -1,37 +1,12 @@
-$(document).on('turbolinks:load',function() {
 $(function(){
-
-  // $(".search_form_side").on("submit", function(){
-  //   let brand_name = $("#q_brand_id_eq").val();
-  //   $.ajax({
-  //     type: "get",
-  //     datatype: "json",
-  //     url: "/select_brand"
-  //   })
-  //   .done(function(){
-  //     console.log("ok")
-  //   })
-  //   .fail(function(){
-
-  //   })
-  // })
-
 
   $(".search_form_side").on("submit", function(){
     brand_name = $("#q_brand_id_eq").val();
     $.cookie('brand_name',brand_name);
   })
 
-  // function get_brand_name()
-
-
-  // let brand_id = brand_name
-
   window.onload = function(){
-    // console.log(brand_id);
     var brand = $.cookie('brand_name');
     $("#q_brand_id_eq").val(brand);
    }
-  
-  })
 })
