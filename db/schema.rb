7849632e78a7ba59f873771d1eeedc27354db28c
yamawo <y7a3m1asaki@gmail.com>
2019-10-28 15:53:08 +0900

    conflict修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_10_19_081908) do
-
+ActiveRecord::Schema.define(version: 2019_10_25_061725) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
