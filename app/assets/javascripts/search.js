0a80d4c3d6a28b037d3d0eaa280130a84efee6ec
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-10-29 20:17:25 +0900

    配送方法がリロードなしで編集ページ表示

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -4,20 +4,21 @@ $(document).on("turbolinks:load", function(){
     var search_result = $(".selling__main__sec__content__form__write__content__box__group__select__search");
     var html = `<li class="selling__main__sec__content__form__write__content__box__group__select__search__ele" data-id="${brand.id}">${brand.name}</li>`
     search_result.append(html);
+    console.log(html)
   }
-  $(document).on('input', "#brand_list", function(){//動的に追加した要素はdocumentで
+  $(document).on('keyup', "#brand_list", function(){//動的に追加した要素はdocumentで
     var input = $('#brand_list').val();
     if (input.length !== 0){
       $.ajax({
-      url: "/products/search",
-      data: { keyword: input },
-      dataType: "json"
+        url: "/products/search",
+        data: { keyword: input },
+        dataType: "json"
       })
       .done(function(brands){
-      $(".selling__main__sec__content__form__write__content__box__group__select__search").empty();
-      if(brands.length !== 0){
-        brands.forEach(function(brand){
-          appendBrand(brand);
+        $(".selling__main__sec__content__form__write__content__box__group__select__search").empty();
+        if(brands.length !== 0){
+          brands.forEach(function(brand){
+            appendBrand(brand);
         })
       }
       })
