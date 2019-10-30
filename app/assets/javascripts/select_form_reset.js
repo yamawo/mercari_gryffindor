20a5cf073413a406ef54b34a542ae9c0398901a4
kohama66 <l.my.me666666@gmail.com>
2019-10-30 12:38:36 +0900

    検索フォーム修正
    詳細検索フォームにて、一度クリアを押すとリロードしなくては検索機能が働かなかったバグを修正する

--- app/assets/javascripts/select_form_reset.js
+++ app/assets/javascripts/select_form_reset.js
@@ -4,6 +4,11 @@ $(function(){
   $(".search_for-main__side-bar--button--reset").on("click", function(e){
     e.preventDefault();
     clearForm(".search_form_side");
+    $(q_name_cont).val("");
+    $(q_category_id).val("");
+    $(q_brand_id_eq).val("");
+    $(q_price_gteq).val("");
+    $(q_price_lteq).val("");
     $(".category_children").remove();
     $(".category_indirects").remove();
   })
@@ -12,7 +17,6 @@ $(function(){
     $(form)
       .find("input, select, textarea")
       .not(":button, :submit, :reset, :hidden")
-      .val("")
       .prop("checked", false)
       .prop("selected", false)
     ;
